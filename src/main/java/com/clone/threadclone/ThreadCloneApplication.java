462980c9446aac801b8861a66a86e4fc1d2ef69f
Akshay Jadhav <akshayjadhav461999@gmail.com>
2024-11-13 18:49:32 +0530

    Init

---
+++ src/main/java/com/clone/threadclone/ThreadCloneApplication.java
@@ -0,0 +1,13 @@
+package com.clone.threadclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clone/threadclone/ThreadCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.threadclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
