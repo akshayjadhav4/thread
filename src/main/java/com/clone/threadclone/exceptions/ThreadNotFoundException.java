85ea674d5fb234bb593697b706a769a1f46fb36f
Akshay Jadhav <akshayjadhav461999@gmail.com>
2024-11-23 12:55:09 +0530

    Created exception classes

---
+++ src/main/java/com/clone/threadclone/exceptions/ThreadNotFoundException.java
@@ -0,0 +1,8 @@
+package com.clone.threadclone.exceptions;
+
+public class ThreadNotFoundException extends RuntimeException {
+
+    public ThreadNotFoundException(String message) {
+        super(message);
+    }
+}
